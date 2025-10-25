4915cf148e951d38e7b387421602d3692226db8e
Rafael Costa <racas1@iscte-iul.pt>
2025-10-25 23:26:37 +0100

    V3

---
+++ src/Ficha3/v3/Customer.java
@@ -0,0 +1,56 @@
+package Ficha3.v3;
+
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector<Rental> _rentals = new Vector<Rental>();
+
+    public Customer(String name) {
+        this._name = name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    /** V1: Long Method -> Extract Method (mantém getAmount em Customer) */
+    public String statement() {
+        double totalAmount = 0.0;
+        int frequentRenterPoints = 0;
+
+        String result = "Rental Record for " + getName() + "\n";
+
+        for (Rental each : _rentals) {
+            // valor desta locação (método extraído em V1)
+            double thisAmount = each.getAmount();
+
+            // calcular pontos de aluguer frequente
+            frequentRenterPoints += getFrequentRentalPoints(each);
+
+            // linha desta locação
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+        // footer
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
+
+    public int getFrequentRentalPoints(Rental each){
+        // Verifica se o filme é um lançamento recente e se foi alugado por mais de 1 dia
+        if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+
+}
